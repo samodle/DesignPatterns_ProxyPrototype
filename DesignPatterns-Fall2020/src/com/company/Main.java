8df622764738537719c7b5584426d13c368ead33
Sam Odle <sro27@cornell.edu>
2020-11-12 21:35:08 -0500

    adapter + prototype pattern

--- DesignPatterns-Fall2020/src/com/company/Main.java
+++ DesignPatterns-Fall2020/src/com/company/Main.java
@@ -1,47 +1,157 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("its working!!!");
+
+        ShoppingCart CartA = new ShoppingCart();
+        ShoppingCart CartB = (ShoppingCart) CartA.clone();
+        ShoppingCart CartC = (ShoppingCart) CartB.clone();
+
+        Tomato tomatoA = new Tomato("Bob the Tomato");
+        Tomato tomatoB = new Tomato("Steve the Tomato");
+        Tomato tomatoC = new Tomato("Larry the Tomato");
+
+        System.out.println("Basic Vegetable Functions:");
+        tomatoB.Decompose();
+        tomatoB.Ripen();
+        System.out.println("");
+
+        System.out.println("Basic Ice Cream Functions:");
+        VanillaBean vbCream = new VanillaBean("Vanilla Bean Ice Cream");
+        vbCream.Freeze();
+        vbCream.Melt();
+        System.out.println("");
+
+        //remember, the cart can only take vegetables so we use the adapter
+        CartC.addItem(tomatoA);
+        CartC.addItem(new IceCreamAdapter(vbCream));
+        CartC.addItem(tomatoC);
+        System.out.println("");
+
+        System.out.println("Let's print them all as vegetables (or vegetable adapters):");
+        CartC.PrintCart();
     }
 }
 
-
 //Adapter Implementation
+interface IceCream {
+    AtomicReference<String> Flavor = new AtomicReference<String>();
+    String getFlavor();
+    void Freeze();
+    void Melt();
+}
 
-interface Socket {
-    String getOutput();
+interface Vegetable{
+    AtomicReference<String> Name = new AtomicReference<String>();
+    String getName();
+    void Ripen();
+    void Decompose();
 }
-class Plug {
-    private String specification = "5 AMP";
-    public String getInput() { return specification; }
-}
-class Adapter implements Socket {
-    public String getOutput(int input) {
-// could use “input” to determine “right” interface/class
-        Plug plug = new Plug();
-        return plug.getInput(); }
+
+class Tomato implements Vegetable{
 
     @Override
-    public String getOutput() {
-        return null;
+    public String getName() {
+        return Name.get();
+    }
+
+    @Override
+    public void Ripen() {
+        System.out.println(Name + " is 10% riper!");
+    }
+
+    @Override
+    public void Decompose() {
+        System.out.println(Name + " is decomposing");
+    }
+
+    public void MakeSalsa(){
+        System.out.println("I'm salsa!");
+    }
+
+    public Tomato(String n){
+        Name.set(n);
+    }
+}
+
+class VanillaBean implements IceCream{
+
+    @Override
+    public String getFlavor() {
+        return Flavor.get();
+    }
+
+    @Override
+    public void Freeze() {
+        System.out.println(Flavor + " is becoming frozen");
+    }
+
+    @Override
+    public void Melt() {
+        System.out.println(Flavor + " is 10% soupier");
+    }
+
+    public void Scoop(){}
+
+    public VanillaBean(String f){
+        Flavor.set(f);
+    }
+}
+
+class IceCreamAdapter implements Vegetable{
+    IceCream iceCream;
+
+    @Override
+    public String getName() {
+        return iceCream.getFlavor();
+    }
+
+    @Override
+    public void Ripen() {
+        iceCream.Freeze(); //yes, i'm aware freezing != ripening
+    }
+
+    @Override
+    public void Decompose() {
+        iceCream.Melt(); //this one is closer
+    }
+
+    public IceCreamAdapter(IceCream i){
+        iceCream = i;
     }
 }
 
 //Prototype Implementation
 abstract class CartPrototype implements Cloneable {
+    static int counter = 1;
+    int cartID;
+    List<Vegetable> Veggies = new ArrayList<Vegetable>();
+
     public abstract CartPrototype clone();
 }
 
 class ShoppingCart extends CartPrototype implements Cloneable {
     public CartPrototype clone() { return new ShoppingCart(); }
+
+    void addItem(Vegetable v){
+        Veggies.add(v);
+    }
+
+    public ShoppingCart(){
+        counter++;
+        cartID = counter;
+    }
+
+    void PrintCart(){
+        for(Vegetable v : Veggies){
+            System.out.println(v.getName());
+            v.Ripen();
+        }
+    }
 }
 
-//class Client {
-   // public static void main(String[] args) {
-// could use input “type” to determine “right” concrete prototype
-   ///     Prototype myPrototype = new ConcretePrototype1();
-   //     Prototype result = myPrototype.clone(); }
-//}
